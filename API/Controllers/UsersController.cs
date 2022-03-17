c8ddbb95fa90c0673022ae25d630b1eee7feedf1
Ibrahim Taofeek <Ibraheem.Taofeek@fintraksoftware.com>
2022-03-17 12:27:10 +0100

    first commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        //Ctrl + . to generate private readonly constructor
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            //var users = _context.Users.ToList();
+            //return users;
+            return await _context.Users.ToListAsync();
+        }
+
+        //api/users/id e.g 1,2,3 e.t.c
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            //var user = _context.Users.Find(id);
+            //return user;
+            return await _context.Users.FindAsync(id);
+        }
+
+
+
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        //ctrl + . on Datacontext to generate constructor
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        //Generates a table called Users, Can be anything of choice
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
