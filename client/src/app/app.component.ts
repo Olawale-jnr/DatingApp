6efb494e21ef46459cc9a557e2884d8fed71c635
Ibrahim Taofeek <Ibraheem.Taofeek@fintraksoftware.com>
2022-03-28 14:38:48 +0100

    Register-Enabled

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,20 +12,17 @@ export class AppComponent implements OnInit{
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
 
-  getUsers(){
-    this.http.get('https://localhost:44396/api/users').subscribe(
-      response => {
-        this.users = response;
-      }, error => {
-        console.log(error);
-      }
-    )
+  setCurrentUser(){
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
+
+  
 }
 
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { AccountService } from './../_services/account.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+ // @Input() usersFromHomeComponent: any;
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+  
+  register(){
+    this.accountService.register(this.model).subscribe(response =>{
+      console.log(response);
+      this.cancel();
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  cancel(){
+    //console.log('cancelled');
+    this.cancelRegister.emit(false)
+  }
+
+}
