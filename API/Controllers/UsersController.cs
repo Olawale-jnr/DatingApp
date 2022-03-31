5d4059d5b3fe27318ae68b1d85b4805cb50a114a
Ibrahim Taofeek <Ibraheem.Taofeek@fintraksoftware.com>
2022-03-31 11:04:51 +0100

    Seeding and Automapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 ﻿using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,35 +13,46 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
+        //private readonly DataContext _context;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
         //Ctrl + . to generate private readonly constructor
-        public UsersController(DataContext context)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+           // _context = context;
+            _userRepository = userRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        //[AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
             //var users = _context.Users.ToList();
             //return users;
-            return await _context.Users.ToListAsync();
+            //var users = await _userRepository.GetUsersAsync();
+            //var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+            //return Ok(usersToReturn);
+            var users = await _userRepository.GetMembersAsync();
+            return Ok(users);
         }
 
         //api/users/id e.g 1,2,3 e.t.c
-        [Authorize]
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        //[Authorize]
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             //var user = _context.Users.Find(id);
             //return user;
-            return await _context.Users.FindAsync(id);
+            //var user = await _userRepository.GetUserByUsernameAsync(username);
+            //return _mapper.Map<MemberDto>(user);
+
+            return await _userRepository.GetMemberAsync(username);
         }
-
-
 
     }
 }
---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,26 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            //Map from source to destination, source is AppUser, Destination is the Dto..
+            //For member, Mapping from source and assigning to destination
+            CreateMap<AppUser, MemberDto>().ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => 
+                src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth
+                .CalculateAge()));
+
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
